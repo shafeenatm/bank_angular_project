efb0a97dfe5b9b08aa5f8d3eb060f78bfb6ee5ed
unknown <shafeenatm2000@gmail.com>
2023-02-02 16:36:47 +0530

    second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,32 +8,65 @@ import { Component } from '@angular/core';
 export class LoginComponent {
   data="an Ideal destination for Personal Banking needs"
   inputplaceholder="Account Number"
-  acno=''
-  psw=''
+  // acno=''
+  // psw=''
   // or any
   userDetails:any={
-    1000:{acno:1000,username:"anu",password:"anu123",balance:0},
-    1001:{acno:1001,username:"ammu",password:"amm123",balance:0},
-    1002:{acno:1001,username:"ansa",password:"ans123",balance:0},
-    1003:{acno:1001,username:"musa",password:"mus123",balance:0},
-    1004:{acno:1001,username:"lathu",password:"lat123",balance:0}
+    1000:{acno:1000,username:"anu",password:"abc123",balance:0},
+    1001:{acno:1001,username:"ammu",password:"abc123",balance:0},
+    1002:{acno:1001,username:"ansa",password:"abc123",balance:0},
+    1003:{acno:1001,username:"musa",password:"abc123",balance:0},
+    1004:{acno:1001,username:"lathu",password:"abc123",balance:0}
   }
   constructor() { }
   ngOnInIt(): void { 
 
   }
+  // login(){
+  //   var acnum=this.acno
+  //   var pasw=this.psw
+  //   var userDetails=this.userDetails
+  //   if(acnum in userDetails){
+  //     if(pasw==userDetails[acnum]["password"]){
+  //       alert("login success")
+  //     }
+  //     else{
+  //       alert("incorrect password")
+  //     }
 
-  login(){
+  //   }
+  //   else{
+  //     alert("user is not registered or incorrect account number")
+  //   }
+  //   // alert('login clicked')
+  // }
+  login(a:any,b:any){
+
+    var acnum=a.value
+    var pasw=b.value
+    var userDetails=this.userDetails
+    if(acnum in userDetails){
+      if(pasw==userDetails[acnum]["password"]){
+        alert("login success")
+      }
+      else{
+        alert("incorrect password")
+      }
+
+    }
+    else{
+      alert("user is not registered or incorrect account number")
+    }
     alert('login clicked')
   }
-  acnoChange(event:any){
-    this.acno=event.target.value;
-    console.log(this.acno);
+//   acnoChange(event:any){
+//     this.acno=event.target.value;
+//     // console.log(this.acno);
     
-  }
-  pswrdChange(event:any){
-    this.psw=event.target.value
-    console.log(this.psw);
+//   }
+//   pswrdChange(event:any){
+//     this.psw=event.target.value
+//     // console.log(this.psw);
     
-}
+// }
 }
